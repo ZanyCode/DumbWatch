b4570fce29830a2af81ff66759fc0595739fb948
Tobias Rempel <tobias.rempel@googlemail.com>
2022-12-23 11:37:23 +0100

    Cleanup of app

--- App/app/src/main/java/org/zanytek/zanytime/DeviceAPI.kt
+++
@@ -1,9 +0,0 @@
-package org.zanytek.zanytime
-
-
-interface DeviceAPI {
-	/**
-	 * Change the value of the GATT characteristic that we're publishing
-	 */
-	fun setMyCharacteristicValue(value: String)
-}
